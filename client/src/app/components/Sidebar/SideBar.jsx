07b41a25eda82b7429182d9a70f82da569459555
Sachin <SachinMourya303@gmail.com>
2025-11-10 17:02:37 +0530

    Save sidebar locally

--- client/src/app/components/Sidebar/SideBar.jsx
+++ client/src/app/components/Sidebar/SideBar.jsx
@@ -1,36 +1,16 @@
 import React, { useEffect } from 'react'
 import Profile from './utils/Profile'
 import Shortcuts from './utils/Shortcuts'
-import { Skeleton } from '@/components/ui/skeleton';
-import { useDispatch, useSelector } from 'react-redux';
-import { useQuery } from '@tanstack/react-query';
-import { setUsers } from '@/app/stateManagement/slice/usersSlice';
-import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { Skeleton } from 'primereact/skeleton';
 
 const SideBar = () => {
-  const users = useSelector(state => state.users.usersData);
-  const dispatch = useDispatch();
 
-  const { data, isPending, error } = useQuery({
-    queryKey: ['users'],
-    queryFn: async () => {
-      const res = await axios.get(`${import.meta.env.VITE_API_URI}/users/user-details`);
-      return res.data;
-    },
-    staleTime: 1000 * 60 * 1,
-    cacheTime: 1000 * 60 * 5,
-    refetchOnWindowFocus: false,
-  });
+  const isLoading = useSelector(state => state.users.isLoading);
 
-  useEffect(() => {
-    if (data?.users) {
-      dispatch(setUsers(data.users));
-    }
-  }, [data, dispatch]);
-
-  if (isPending) return <div className='flex flex-col items-center justify-center gap-5 m-5'>
-    <Skeleton className='h-80 w-full hidden md:flex' />
-    <Skeleton className='h-80 w-full hidden md:flex' />
+  if (isLoading) return <div className='flex flex-col items-center justify-center gap-5 m-5'>
+    <Skeleton className='h-80! w-full hidden md:flex' />
+    <Skeleton className='h-80! w-full hidden md:flex' />
   </div>
 
   return (
