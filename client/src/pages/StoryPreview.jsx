d8a5f6f9ae78658cb5744b11917b1188136cec4a
Sachin <SachinMourya303@gmail.com>
2025-11-10 21:01:08 +0530

    Added the ui and logic of delete story

--- client/src/pages/StoryPreview.jsx
+++ client/src/pages/StoryPreview.jsx
@@ -9,14 +9,15 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 const StoryPreview = () => {
   const users = useSelector(state => state.users.usersData);
+  const { userDetails, darkmode } = useSelector(state => state.userAuth);
 
   const navigate = useNavigate();
 
   const { storyId } = useParams();
 
   const findUser = users.find((user) => user._id === storyId);
-  console.log(findUser);
-  
+  console.log(findUser._id);
+
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -49,11 +50,13 @@ const StoryPreview = () => {
           : <video src={findUser?.storyFile?.url} alt='story' autoPlay muted playsInline loop className='w-full h-full object-cover object-center' />
         }
 
-        <div className='absolute flex items-center w-full justify-between top-5 left-0 md:top-10 px-10'>
+        <div className='absolute flex items-center w-full justify-between top-5 left-0 md:top-10 md:px-10 h-10'>
           <Button onClick={() => navigate('/')} className='bg-transparent hover:bg-transparent md:bg-app-theme md:hover:bg-app-theme/50 rounded-full  p-0 w-10 h-10 md:h-20 md:w-20 cursor-pointer'>
             <ChevronLeft className='size-10' />
           </Button>
-          <div onClick={() => deleteStory(findUser?._id)} className='bg-red-500/50 hover:bg-red-500/80 text-white cursor-pointer p-3 rounded-full'><Trash /></div>
+          {findUser._id === userDetails.users._id
+            ? <div onClick={() => deleteStory(findUser?._id)} className='bg-red-500/50 hover:bg-red-500/80 text-white cursor-pointer p-3 rounded-full mr-2 md:mr-0'><Trash /></div>
+            : ""}
         </div>
       </div>
     </div>
