503b8cfcab42b06cab4fca75d58ef6d5c18006e2
Sachin <SachinMourya303@gmail.com>
2025-11-14 18:46:25 +0530

    Updated the delete function

--- client/src/utils/deletePost.js
+++ client/src/utils/deletePost.js
@@ -2,15 +2,17 @@ import { setFollowButtonLoading } from "@/app/stateManagement/slice/usersSlice";
 import axios from "axios";
 import toast from "react-hot-toast";
 
- export const sendDeletePostRequest = async ( dispatch, postId ) => {
-    dispatch(setFollowButtonLoading(true));
-    try {
-      const response = await axios.delete(`${import.meta.env.VITE_API_URI}/post/delete`, { postId });
-      toast.success(response.data.message);
-    } catch (error) {
-      toast.error(error.message || 'Something went wrong');
-    }
-    finally{
-        dispatch(setFollowButtonLoading(false));
-    }
+ export const sendDeletePostRequest = async (dispatch, postId) => {
+  dispatch(setFollowButtonLoading(true));
+  try {
+    const response = await axios.delete(
+      `${import.meta.env.VITE_API_URI}/post/delete`,
+      { data: { postId } }
+    );
+    toast.success(response.data.message);
+  } catch (error) {
+    toast.error(error.response?.data?.message || 'Something went wrong');
+  } finally {
+    dispatch(setFollowButtonLoading(false));
   }
+};
