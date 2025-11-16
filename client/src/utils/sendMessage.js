f8d9394227cf3177c37bbafdb383f72e8988407c
Sachin <SachinMourya303@gmail.com>
2025-11-16 17:08:34 +0530

    Updated the fetch data routes

--- client/src/utils/sendMessage.js
+++ client/src/utils/sendMessage.js
@@ -1,15 +1,29 @@
 import { setFollowButtonLoading } from "@/app/stateManagement/slice/usersSlice";
 import axios from "axios";
+import toast from "react-hot-toast";
 
  export const sendMessageRequest = async ( dispatch, roomId, senderId, receiverId, message , setMessage ) => {
     dispatch(setFollowButtonLoading(true));
     try {
       const response = await axios.post(`${import.meta.env.VITE_API_URI}/message/send`, { roomId, senderId, receiverId, message });
-      toast.success(response.data.message);
     } catch (error) {
+        toast.error(error.message);
     }
     finally{
         dispatch(setFollowButtonLoading(false));
         setMessage('');
+    }
+  }
+
+   export const fetchMessageRequest = async (dispatch , setChat , roomId ) => {
+    dispatch(setFollowButtonLoading(true));
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_API_URI}/message/chatroom`, {params: { roomId }});
+      setChat(response.data);
+    } catch (error) {
+        toast.error(error.message);
+    }
+    finally{
+        dispatch(setFollowButtonLoading(false));
     }
   }
